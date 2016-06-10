8f08074e7da9c4ba5fa2986bbd6cddfcafa26db9
alexakos <xenofon.alexakos@nokia.com>
2016-06-10 16:32:03 +0300

    Initial Design

---
+++ index.js
@@ -0,0 +1,21 @@
+function ready() {
+    document.getElementById("page-1").style.display = "block";
+};
+
+function changePage(hideId, showId) {
+   var hideElement, showElement;
+   hideElement = document.getElementById(hideId);
+   showElement = document.getElementById(showId);
+   showElement.style.display = "block";
+   hideElement.style.display= "none";
+};
+
+function showPopUp (src, num){
+
+    var popUpElement;
+    popUpElement = document.getElementById("popup");
+    popUpElement.childNodes[0].src = src;
+    popUpElement.style.display = 'block';
+    setTimeout(function () {popUpElement.style.display = 'none';}, 2000);
+
+};
