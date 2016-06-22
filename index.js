7946661c66ddde9e404f213b840fbfb8d425ca9e
belafonde <arapakisj@hotmail.com>
2016-06-22 20:27:14 +0300

    drawer

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 clickCount = 1;
 equip = 0;
+box1 = 1;
+box2 = 2;
+box3 = 3;
+box4 = 4;
 
 function ready() {
     document.getElementById("page-1").style.display = "block";
@@ -25,6 +29,8 @@ function changePage(hideId, showId) {
 		showElement.style.display = "block";
 		supportElemnt.style.display = "none";
 		supportElemnt.style.marginLeft = "0px";
+		support1Elemnt.innerHTML= '';
+		support2Elemnt.innerHTML= '';
    }, 2000);
 };
 
@@ -126,3 +132,141 @@ function changeVentilatorImage (img) {
 	};
 	
 }
+
+
+function colorSelector(boxDiv){
+	if (boxDiv.id == "colorBox1" ) {
+		switch(box1) {
+			case 1:
+				box1=2;
+				boxDiv.style.backgroundImage = "url('images/greenBox.png')"
+				break;
+			case 2:
+				box1=3;
+				boxDiv.style.backgroundImage = "url('images/redBox.png')"
+				break;
+			case 3:
+				box1=4;
+				boxDiv.style.backgroundImage = "url('images/blueBox.png')"
+				break;
+			case 4:
+				box1=1;
+				boxDiv.style.backgroundImage = "url('images/yellowBox.png')"
+				break;
+			default:
+				break;
+		}
+		
+	}else if(boxDiv.id == "colorBox2" ){
+		switch(box2) {
+			case 1:
+				box2=2;
+				boxDiv.style.backgroundImage = "url('images/greenBox.png')"
+				break;
+			case 2:
+				box2=3;
+				boxDiv.style.backgroundImage = "url('images/redBox.png')"
+				break;
+			case 3:
+				box2=4;
+				boxDiv.style.backgroundImage = "url('images/blueBox.png')"
+				break;
+			case 4:
+				box2=1;
+				boxDiv.style.backgroundImage = "url('images/yellowBox.png')"
+				break;
+			default:
+				break;
+		}
+		
+	
+	}else if(boxDiv.id == "colorBox3" ){
+		switch(box3) {
+			case 1:
+				box3=2;
+				boxDiv.style.backgroundImage = "url('images/greenBox.png')"
+				break;
+			case 2:
+				box3=3;
+				boxDiv.style.backgroundImage = "url('images/redBox.png')"
+				break;
+			case 3:
+				box3=4;
+				boxDiv.style.backgroundImage = "url('images/blueBox.png')"
+				break;
+			case 4:
+				box3=1;
+				boxDiv.style.backgroundImage = "url('images/yellowBox.png')"
+				break;
+			default:
+				break;
+		}
+		
+	
+	}else if(boxDiv.id == "colorBox4" ){
+		switch(box4) {
+			case 1:
+				box4=2;
+				boxDiv.style.backgroundImage = "url('images/greenBox.png')"
+				break;
+			case 2:
+				box4=3;
+				boxDiv.style.backgroundImage = "url('images/redBox.png')"
+				break;
+			case 3:
+				box4=4;
+				boxDiv.style.backgroundImage = "url('images/blueBox.png')"
+				break;
+			case 4:
+				box4=1;
+				boxDiv.style.backgroundImage = "url('images/yellowBox.png')"
+				break;
+			default:
+				break;
+		}
+	}
+};
+
+function checkcolors(){
+	var tableDiv;
+	if (box1==3 && box2==4 && box3==4 && box4==2){
+		document.getElementById("keyimage").style.display="block";
+		document.getElementById("checkButton").onclick=null;
+		document.getElementById("checkButton").style.cursor = "default";
+		tableDiv = document.getElementById("table");
+		document.getElementById("table").childNodes[0].src = "images/table_with_pc.png";
+		tableDiv.childNodes[0].onclick = function() {
+			changePage("page-1", "pcPage");
+		};
+	}
+};
+
+function showKey(){
+	
+};
+
+function takeKey(){
+
+    var popUpElement, inventoryElement;
+
+    popUpElement = document.getElementById("popup");
+    popUpElement.childNodes[0].src = "images/key.png";
+    popUpElement.style.display = 'block';
+    inventoryElement = document.getElementById("key");
+	setTimeout(function () {
+		popUpElement.style.marginTop = "200px";
+		popUpElement.style.marginLeft = "200px";
+		popUpElement.style.width = "400px";
+		popUpElement.style.height = "300px";
+	}, 10);
+    setTimeout(function () {
+		popUpElement.style.display = 'none';
+		popUpElement.style.width = "0px";
+		popUpElement.style.height = "0px";
+		popUpElement.style.marginLeft = "400px";
+		popUpElement.style.marginTop = "350px";
+		inventoryElement.style.display = "block";
+	}, 2000);
+	document.getElementById("keyimage").style.display = "none";
+
+};
